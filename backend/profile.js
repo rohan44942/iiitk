710c57c1ce7d5b35aa7485011fdc4269388bd945
rohan <rohannooniwal39@gmail.com>
2024-09-11 18:59:01 +0530

    After pulling and checking the code for backen

--- backend/profile.js
+++ backend/profile.js
@@ -12,6 +12,7 @@ router.post("/", (req, res) => {
     console.log(req.files);
     res.status(200).json({ message: "ok" });
   } catch {
+    
     throw new Error("Error in the avatar and res.status");
   }
 });
